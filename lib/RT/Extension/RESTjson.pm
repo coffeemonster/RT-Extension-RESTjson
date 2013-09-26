d57325fff6d275de2721a9cc1b40d0408ab985cb
Alister West <alister@alisterwest.com>
2013-09-26 15:59:31 -0700

    version 1.2 uses 1.0/Forms

--- lib/RT/Extension/RESTjson.pm
+++ lib/RT/Extension/RESTjson.pm
@@ -1,9 +1,20 @@
 package RT::Extension::RESTjson;
-our $VERSION = "1.2";
+our $VERSION = "1.0";
 1;
 =head1 DESCRIPTION
 
-    RT::Extension::RESTjson - json interface to the REST/1.0 interface
+    RT::Extension::RESTjson - json overlay for the REST/1.0 interface
+
+=head1 SYNOPSIS
+
+    The following interfaces have been redefined:
+
+        GET /REST/1.2/ticket/nn/*
+        GET /REST/1.2/search/ticket/
+
+    Todo: POST (and the error handling for them)
+
+    Note: v1.2 incase RT comes out with a 1.1
 
 =head1 AUTHOR
 
