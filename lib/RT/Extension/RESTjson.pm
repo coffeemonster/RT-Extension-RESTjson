5dacb82e833e02c26bf66fe62caecbc24834b8e7
Alister West <alister@alisterwest.com>
2013-09-26 15:28:16 -0700

    initial commit

---
+++ lib/RT/Extension/RESTjson.pm
@@ -0,0 +1,12 @@
+package RT::Extension::RESTjson;
+our $VERSION = "1.2";
+1;
+=head1 DESCRIPTION
+
+    RT::Extension::RESTjson - json interface to the REST/1.0 interface
+
+=head1 AUTHOR
+
+    alisterwest.com
+
+=cut
