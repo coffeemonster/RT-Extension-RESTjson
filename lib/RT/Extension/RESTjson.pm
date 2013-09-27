7c1087025f1bbce97ed2c0c4d959772dd4a9d6db
Alister West <alister@alisterwest.com>
2013-09-26 18:39:54 -0700

    rename tickets -> objects. examples in docs

--- lib/RT/Extension/RESTjson.pm
+++ lib/RT/Extension/RESTjson.pm
@@ -7,17 +7,57 @@ our $VERSION = "1.0";
 
 =head1 SYNOPSIS
 
-    The following interfaces have been redefined:
+    The following interfaces have been redefined to make responses easier parse:
 
-        GET /REST/1.2/ticket/nn/*
         GET /REST/1.2/search/ticket/
+        GET /REST/1.2/ticket/nn/
+        GET /REST/1.2/ticket/nn/history
+        GET /REST/1.2/ticket/nn/attachments
+        GET /REST/1.2/ticket/nn/attachments/xx
 
-    Todo: POST (and the error handling for them)
+    JSON wrapper: all responses come in this form
 
-    Note: v1.2 in case BestPractical comes out with a 1.1
+        {
+            "status" => "200 Ok",
+            "version" => "RT/4.0.12",
+            "objects" => [
+                { <object_1> }, { <object_2> }, ...
+            ],
+            # error only exists if there was an error
+            "error" => "error message"
+        }
+ 
+
+=head1 OBJECTS
+
+    GET /REST/1.2/search/ticket?query=Requestor.EmailAddress%20LIKE%20"alister@example.com";fields=id,Subject,Created
+    <object> : { "id" : "11", "Subject" : "My Test Ticket!", "Created" : "Wed May 29 21:08:58 2013" }
+
+    GET /REST/1.2/ticket/123/
+    <object> : { "id" : "185", "object" : "ticket/11", "Subject" : "My Subject"  .. }
+
+    GET /REST/1.2/ticket/123/attachments
+    <object> :  "id" : "185",
+                "object" : "ticket/11",
+                "Attachments" : [
+                    "183: (Unnamed) (multipart/mixed / 0b)",
+                    "184: (Unnamed) (text/html / 29b)",
+                    "185: #5778.pdf (application/pdf / 112.2k)"
+                ]
+
+    GET /REST/1.2/ticket/123/attachments/185 
+    <object> :  "id" : "185",
+                "object" : "ticket/11/attachments/185",
+                "Headers" : "Subject: first post!\n...",
+                "Content-Type" : "application/pdf","
+                "Content" : "%PDF-1.4\r\n...",
+                ...
 
 =head1 AUTHOR
 
     Alister West <https://alisterwest.com>
+
+    Todo: POST (and the error handling for them)
+    Note: v1.2 in case BestPractical comes out with a 1.1
 
 =cut
